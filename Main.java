91b216aab14f4527873a0d074f929c8f08d884cb
ehshamhussain786 <123303146+ehshamhussain786@users.noreply.github.com>
2023-01-22 17:19:58 +0500

    first  commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+public class Main {
+	
+	public Main() {
+		JFrame frame = new JFrame();
+		Gamepanel gamepanel = new Gamepanel();
+       
+       
+        frame.add(gamepanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("SNAKE CODING BY EHSHAM AND THE GROUP");
+        // frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);     
+	}
+	
+
+
+
+	public static void main(String[] args) {
+		new Main();
+
+	}
+
+}
